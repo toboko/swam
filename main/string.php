c3ba90fc26601cbaad1b385fa8a073a4043a5ced
Nicola Bombaci <sonybombaci@gmail.com>
2015-06-27 11:00:26 +0200

    add /main path

---
+++ main/string.php
@@ -0,0 +1,15 @@
+<?php
+echo '<html>';
+echo '<head>';
+echo '<title>HI SWAM</title>';
+echo '</head>';
+echo '<body>';
+$max = 3;$max2 = 5;$totale = $max + $max2;echo $totale;echo '<nav id="navigation">';
+echo '<ul>';
+echo '<li>Ciao</li>';
+echo '<li>Menu</li>';
+echo '<li>Get This</li>';
+echo '</ul>';
+echo '</nav>';
+echo '</body>';
+echo '</html>';
